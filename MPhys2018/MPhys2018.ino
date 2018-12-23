0fc867498212548d0a7c9dc28ad09e73e295643a
George webb <George_Webb@outlook.com>
2018-12-23 19:30:15 +0000

    Correct the scope of many get_angle variables and improve  comenting

--- MPhys2018/MPhys2018.ino
+++ MPhys2018/MPhys2018.ino
@@ -11,28 +11,30 @@
 #define APin 7
 #define pwmPin 3
 
-// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
+// An MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
 MPU9250 IMU(Wire, 0x68);
-int status; //used by MPU9250 for error checking
 
-//controls loop switch
+// Used by MPU9250 for error checking
+int status; 
+
+// Controls loop switch
 int switchByte;
 
+// Time at which get_angle was last ran in micro seconds (overflow after approximately 70 minutes)
 long TimePrev = 0;
-float AccelX;
-float AccelZ;
-float AccelAngle;
-float GyroY;
-float GyroYCal;
-float GyroAngle;
+
+// Calibration value for Y axis gyro data, established at setup
+float GyroYCal; 
+
+// 'Current' angle value in X-Z plane
 float Angle;
 
+// Current motor speed [-1,1]
 int speed = 0;
-float FuncSpeed = 1;
 
-// the setup function runs once when you press reset or power the board
+// The setup function runs once when you press reset or power the board
 void setup() {
-	//INitialise serial to display angle data
+	//Initialise serial to display angle data
 	Serial.begin(115200);
 	while (!Serial) {}
 
@@ -42,7 +44,7 @@ void setup() {
 	pinMode(pwmPin, OUTPUT);
 
 
-	// start communication with IMU 
+	// Start communication with IMU 
 	status = IMU.begin();
 	if (status < 0) {
 		Serial.println("IMU initialization unsuccessful");
@@ -50,22 +52,18 @@ void setup() {
 		Serial.println(status);
 		while (1) {}
 	}
+	
+	// Calibrate the gyro (Sensor must be stationary for accuracy)
 	IMU.readSensor();
-	//Establish initial angle reading from accelerometer only (Sensor must be stationary for accuracy)
-	TimePrev = micros();
-	AccelX = IMU.getAccelX_mss() / 9.81;
-	AccelZ = IMU.getAccelZ_mss() / 9.81;
-	Angle = atan2f((float)AccelZ, (float)AccelX);
-	//Calibrate the gyro (Sensor must be stationary for accuracy)
 	GyroYCal = IMU.getGyroY_rads();
 }
 
-// the loop function runs over and over again until power down or reset
+// The loop function runs repeatedly until power down or reset
 void loop() {
 	if (Serial.available() > 0) {
 		switchByte = Serial.read();
 	}
-	get_angle();
+	//get_angle();
 	switch (switchByte) 
 	{
 	case 'r':
@@ -83,6 +81,7 @@ void loop() {
 }
 
 void set_speed(float Speed) {
+	//if speed is less than zero, set direction 
 	if (Speed < 0) {
 		digitalWrite(BPin, LOW);
 		digitalWrite(APin, HIGH);
@@ -96,35 +95,38 @@ void set_speed(float Speed) {
 }
 
 void get_angle() {
-	// read the sensor
+	// Read the MPU9250 sensor
 	IMU.readSensor();
 
 	// Normalise accelerometer values in accordance with gravity, important as these values will be combined using 'atan2f' to increase the angles accuracy
-	AccelX = constrain(IMU.getAccelX_mss(), -9.81,9.81) / 9.81;
-	AccelZ = constrain(IMU.getAccelZ_mss(), -9.81, 9.81) / 9.81;
+	float AccelX = constrain(IMU.getAccelX_mss(), -9.81,9.81) / 9.81;
+	float AccelZ = constrain(IMU.getAccelZ_mss(), -9.81, 9.81) / 9.81;
 
 	// Calculate (accel) angle in x-z plane using normalised x and z accelerometer values
-	AccelAngle = atan2f((float)AccelZ, (float)AccelX);
+	float AccelAngle = atan2f((float)AccelZ, (float)AccelX);
 
 	// Correct gyro data using calibration value from setup
-	GyroY = IMU.getGyroY_rads() - GyroYCal;
+	float GyroY = IMU.getGyroY_rads() - GyroYCal;
 
-	
+	//Initalise time values for linear approx of gyro differentiation
 	long TimeNow = micros();
-	long TimeDelta = TimeNow - TimePrev;
-	
+	long TimeDelta = TimeNow - TimePrev;	
+	float GyroAngle = NAN;
+
+	// If time since get_angle was last ran is greater than 0.2 seconds, do not us gyro data as time interval too great for linear approxmation of gyro differatiation
 	if (TimeDelta > 200000) {
-		GyroAngle = AccelAngle;
+		// Set angle to Accelerometer angle
+		Angle = AccelAngle;
 	}
 	else {
 		// Calculate (gyro) angle using Y axis gyro values
 		GyroAngle = Angle + GyroY * TimeDelta / 1000000;
+		// Calculate angle using complamentary filter
+		Angle = 0.98 * GyroAngle + 0.02 * AccelAngle;
 	}
 
-	//Set previous time to be current time, used if/ when this function is run again
+	// Set previous time to be current time, used if/ when this function is run again
 	TimePrev = TimeNow;
 
-
-	Angle = 0.98 * GyroAngle + 0.02 * AccelAngle;
 }
 
