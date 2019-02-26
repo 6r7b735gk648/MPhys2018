422a90af8358bb0206ac454cb6e963baf34fee5c
George webb <George_Webb@outlook.com>
2019-02-26 14:24:18 +0000

    Major code restyling and ux improvements

--- MPhys2018/MPhys2018.ino
+++ MPhys2018/MPhys2018.ino
@@ -1,11 +1,8 @@
-/*
- Name:		MPhys2018.ino
- Created:	12/13/2018 11:38:35 AM
- Author:	George
-*/
-
 #include <math.h>
 #include <MPU9250.h>
+
+// An MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
+MPU9250 IMU(Wire, 0x68);
 
 //Defining OUTPUT pins
 #define BPin 8
@@ -20,21 +17,12 @@ float Gain_Rotor_Speed = -0.;
 float MotorSpeed = 0;
 float PIDOutput = 0;
 
-// An MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-MPU9250 IMU(Wire, 0x68);
-
-// Used by MPU9250 for error checking
-int status; 
-
 // Controls loop switch
 int switchByte = 'c';
 
 // Time at which get_angle was last ran in micro seconds (overflow after approximately 70 minutes)
 long TimePrevGetAngle = 0;
 
-// Calibration value for Y axis gyro data, established at setup
-float GyroAngle = 0;
-float GyroYCal = 0; 
 
 // 'Current' angle value in X-Z plane
 float Angle = 0;
@@ -51,26 +39,19 @@ float AngleProportional = 0;
 float AngleIntegral = 0;
 float AngleDerivative = 0;
 
-// Current motor speed [-1,1]
-int speed = 0;
 // Max motor speed
-float MaxSpeed = 1;
-
+# define MaxSpeed 1
+# define gravity 9.8071495
 // The setup function runs once when you press reset or power the board
 void setup() {
+	
+	//Initialise serial For debug use
+	Serial.begin(115200);
 	Serial.setTimeout(25);
-	//Initialise serial to display angle data
-	//Serial.begin(115200);
-	Serial.begin(56000);
 	while (!Serial) {}
 
-	//Initalise OUTPUT pins
-	pinMode(BPin, OUTPUT);
-	pinMode(APin, OUTPUT);
-	pinMode(pwmPin, OUTPUT);
-
-
-	// Start communication with IMU 
+	// Start communication with MPU9255
+	int status;
 	status = IMU.begin();
 	if (status < 0) {
 		Serial.println("IMU initialization unsuccessful");
@@ -80,8 +61,40 @@ void setup() {
 	}
 	
 	// Calibrate the gyro (Sensor must be stationary for accuracy)
-	IMU.readSensor();
-	GyroYCal = IMU.getGyroY_rads();
+	status = IMU.calibrateGyro();
+	if (status < 0) {
+		Serial.println("Gyro calibration unsuccessful");
+		Serial.print("Error Status: ");
+		Serial.println(status);
+		while (1) {}
+	}
+
+	// Set full scale range of accelerometer to [-2,2] g 
+	status = IMU.setAccelRange(MPU9250::ACCEL_RANGE_2G);
+	if (status < 0) {
+		Serial.println("Gyro calibration unsuccessful");
+		Serial.print("Error Status: ");
+		Serial.println(status);
+		while (1) {}
+	}
+
+	// Set full scale range of gyroscope to [-500,500] deg/s
+	status = IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);
+	if (status < 0) {
+		Serial.println("Set gyroscope range unsuccessful");
+		Serial.print("Error Status: ");
+		Serial.println(status);
+		while (1) {}
+	}
+
+	// Set Digital Low Pass Filter (DLPF) bandwidth to 20 Hz
+	status = IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
+	if (status < 0) {
+		Serial.println("Set digital low pass filter unsuccessful");
+		Serial.print("Error Status: ");
+		Serial.println(status);
+		while (1) {}
+	}
 }
 
 // The loop function runs repeatedly until power down or reset
@@ -89,7 +102,23 @@ void loop() {
 	//long StartTime = micros();
 	if (Serial.available() > 0) {
 		switchByte = Serial.read();
-		Serial.write(switchByte);
+		switch (switchByte)
+		{
+		case 'r':
+			Serial.println("Reading angle data (running get_angle)");
+			break;
+		case 'c':
+			Serial.println("Calibrating setpoint...");
+			break;
+		case 'g':
+			Serial.println("Running PID stabilisation...");
+			break;
+		case 's':
+			Serial.println("STOP!");
+			break;
+		default:
+			break;
+		}
 	}
 
 	
@@ -140,8 +169,8 @@ float pid_controller() {
 	float Error = Angle - SetPointAngle;
 	float TimeDelta = (AngleTime - AngleTimePrev) / 1000000;
 
-	if (abs(Angle - SetPointAngle) >= 0.174533) {
-		
+	if (abs(Error) >= 0.174533) {
+		Serial.println("Error: Angle out of bounds");
 		switchByte = 's';
 		return 0;
 	}
@@ -151,8 +180,6 @@ float pid_controller() {
 	AngleDerivative = Gain_Derivative * (Error - (AnglePrev - SetPointAngle)) / TimeDelta;
 	PIDOutput = AngleProportional + AngleIntegral + AngleDerivative + Gain_Rotor_Speed * PIDOutput;
 
-	// Convert PIDOutput which is 'acceleration' and adding it to 90% of the motor speed to help slow the wheel down if stable, then normalised [-1,1] as a motor speed for PWM control
-	///MotorSpeed = constrain(PIDOutput + MotorSpeed, -MaxSpeed, MaxSpeed);
 	MotorSpeed = constrain(PIDOutput, -MaxSpeed, MaxSpeed);
 
 	return MotorSpeed;
@@ -161,18 +188,18 @@ float pid_controller() {
 void get_angle() {
 	AnglePrev = Angle;
 	AngleTimePrev = TimePrevGetAngle;
+
 	// Read the MPU9250 sensor
-	IMU.readSensor();
+	if (IMU.readSensor() < 0) {
+		Serial.println("Error: No data in MPU9255s buffer");
+	}
 
 	// Normalise accelerometer values in accordance with gravity, important as these values will be combined using 'atan2f' to increase the angles accuracy
-	float AccelX = constrain(IMU.getAccelX_mss(), -9.81,9.81) / 9.81;
-	float AccelZ = constrain(IMU.getAccelZ_mss(), -9.81, 9.81) / 9.81;
+	float AccelX = constrain(IMU.getAccelX_mss(), -gravity, gravity) / gravity;
+	float AccelZ = constrain(IMU.getAccelZ_mss(), -gravity, gravity) / gravity;
 
 	// Calculate (accel) angle in x-z plane using normalised x and z accelerometer values
 	float AccelAngle = atan2f(AccelZ, AccelX);
-
-	// Correct gyro data using calibration value from setup
-	float GyroY = IMU.getGyroY_rads() - GyroYCal;
 
 	//Initalise time values for linear approx of gyro differentiation
 	AngleTime = micros();
@@ -185,14 +212,10 @@ void get_angle() {
 	}
 	else {
 		// Calculate (gyro) angle using Y axis gyro values
-		//GyroAngle = Angle + GyroY * TimeDelta / 1000000;
-		GyroAngle = GyroAngle + GyroY * TimeDelta;
-		Angle = Angle + GyroY*TimeDelta;
+		Angle = Angle + IMU.getGyroY_rads()*TimeDelta;
 		// Calculate angle using complamentary filter
 		Angle = 0.98 * Angle + 0.02 * AccelAngle;
-
 	}
-
 	// Set previous time to be current time, used if/ when this function is run again
 	TimePrevGetAngle = AngleTime;
 	
